0b26f593902daa4ab74582e2f9efa3f288eb6ab9
exoter <exoter@esa.int>
2017-05-04 16:08:22 +0200

    Changed type to be compatible with ROCK master Frame type

--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -6,7 +6,7 @@
 #include <opencv/cv.h>
 #include <opencv2/highgui/highgui.hpp>
 
-#include <base/samples/frame.h>
+#include <base/samples/Frame.hpp>
 #include "cartographer/TaskBase.hpp"
 #include <frame_helper/FrameHelper.h>
 
