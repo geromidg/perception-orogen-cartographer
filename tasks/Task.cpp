f8e7e0988c91dce82b46397cc80d92a022e4bcce
Dimitris Geromichalos <geromidg@gmail.com>
2017-11-15 21:18:48 +0100

    Change camera parameters and transformations

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -26,20 +26,24 @@ bool Task::configureHook()
     if (! TaskBase::configureHook())
         return false;
 
-    // set algorithm parameters
     local_map.setCameraParameters(1024, 768,
-        509.74846, 376.99711, 837.98636, 838.56569);
+        // 509.74846, 376.99711, 837.98636, 838.56569);
+        652, 474, 1419, 1422);
     local_map.setMapParameters(5.0, 0.03);
-    local_map.setPcFiltersParameters(0.015, 20, true);
+    local_map.setPcFiltersParameters(15, 20, true);  // Change after cropping
+    // local_map.setPcFiltersParameters(0.015, 20, true);
     local_map.setPcLimitsParameters(
         Eigen::Vector4f(0.0, -2.0, -1.5, 0.0),
         Eigen::Vector4f(4.0, 2.0, 1.5, 0.0));
 
-    // set camera geometric parameters
-    camera_to_ptu = Eigen::Vector3d(0.0, 0.06, 0.02);
+    // camera_to_ptu = Eigen::Vector3d(0.0, 0.06, 0.02);
+    // body_rotation_offset =  Eigen::Vector3d(0.0, 0.0, 0.0);
+    // ptu_to_center = Eigen::Vector3d(0.09, 0.0, 0.9);
+    // ptu_rotation_offset = Eigen::Vector3d(0.0, -0.07, 0.0);
+    camera_to_ptu = Eigen::Vector3d(0.0, 0.0, 0.0);
     body_rotation_offset =  Eigen::Vector3d(0.0, 0.0, 0.0);
-    ptu_to_center = Eigen::Vector3d(0.09, 0.0, 0.9);
-    ptu_rotation_offset = Eigen::Vector3d(0.0, -0.07, 0.0);
+    ptu_to_center = Eigen::Vector3d(0.058, 0.073, 0.527);
+    ptu_rotation_offset = Eigen::Vector3d(0.0, 0.58, 0.0);
 
     return true;
 }
@@ -58,28 +62,32 @@ void Task::updateHook()
 
     if(_distance_image.read(distance_image) == RTT::NewData)
     {
+        // Eigen::Affine3d tf;
+        // if (!_left_camera_viso22body.get(distance_image.time, tf, false))
+            // std::cout << "[Cartographer] Error getting TF!" << std::endl;
+
         _pose_imu.read(pose_imu);
         double roll = -pose_imu.getRoll();
         double pitch = -pose_imu.getPitch();
 
         Eigen::Quaterniond attitude = Eigen::Quaternion <double> (
                 Eigen::AngleAxisd(body_rotation_offset[0],
-                Eigen::Vector3d::UnitZ()) *
-                    Eigen::AngleAxisd(body_rotation_offset[1] + pitch,
-                Eigen::Vector3d::UnitY()) *
-                    Eigen::AngleAxisd(body_rotation_offset[2] + roll,
-                Eigen::Vector3d::UnitX()));
+                    Eigen::Vector3d::UnitZ()) *
+                Eigen::AngleAxisd(body_rotation_offset[1] + pitch,
+                    Eigen::Vector3d::UnitY()) *
+                Eigen::AngleAxisd(body_rotation_offset[2] + roll,
+                    Eigen::Vector3d::UnitX()));
 
         Eigen::Quaterniond ptu_attitude = Eigen::Quaternion <double> (
                 Eigen::AngleAxisd(ptu_rotation_offset[0],
-                Eigen::Vector3d::UnitZ()) *
-                    Eigen::AngleAxisd(ptu_rotation_offset[1] + (M_PI / 2),
-                Eigen::Vector3d::UnitY()) *
-                    Eigen::AngleAxisd(ptu_rotation_offset[2],
-                Eigen::Vector3d::UnitX()));
+                    Eigen::Vector3d::UnitZ()) *
+                Eigen::AngleAxisd(ptu_rotation_offset[1],
+                    Eigen::Vector3d::UnitY()) *
+                Eigen::AngleAxisd(ptu_rotation_offset[2],
+                    Eigen::Vector3d::UnitX()));
 
         local_map.distance2pointCloud(distance_image.data);
-        local_map.pointColudFiltering();
+        local_map.pointCloudFiltering();
         local_map.pointCloud2flatRobotReference(attitude, ptu_to_center,
             ptu_attitude, camera_to_ptu);
         local_map.pointCloud2heightMap();
@@ -91,14 +99,15 @@ void Task::updateHook()
         customImg = customCVconversion(local_map.getHeightMapInterp());
         _heightMapInterp_frame.write(customImg);
 
-        /* pcl::PointCloud<pcl::PointXYZ>::Ptr p1 = local_map.getPointCloud(); */
-        /* pcl::PointCloud<pcl::PointXYZ>::Ptr p2 = local_map.getPointCloudFiltered(); */
-        /* base::samples::Pointcloud pp1; */
-        /* base::samples::Pointcloud pp2; */
-        /* fromPCLPointCloud(pp1, *p1, 1); */
-        /* fromPCLPointCloud(pp2, *p2, 1), */
-        /* _pointcloud_in.write(pp1); */
-        /* _pointcloud_filter.write(pp2); */
+        // pcl::PointCloud<pcl::PointXYZ>::Ptr p1 = local_map.getPointCloud();
+        // base::samples::Pointcloud pp1;
+        // fromPCLPointCloud(pp1, *p1, 1);
+        // _pointcloud_in.write(pp1);
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr p2 = local_map.getPointCloudFiltered();
+        base::samples::Pointcloud pp2;
+        fromPCLPointCloud(pp2, *p2, 1),
+        _pointcloud_filter.write(pp2);
     }
 }
 
